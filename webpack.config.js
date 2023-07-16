7231f76cd848df31e8364b3df1c5753c6a32e4ee
Estelope <elopez14567@gmail.com>
2023-07-16 00:58:30 -0600

    add additional plugin

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,12 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  devtool: 'eval-source-map',
   plugins: [
-    new CleanWebpackPlugin(), // new line
+    new CleanWebpackPlugin({
+      verbose: true
+    }) , 
+    // above line tells u what files are removed or added when "npm run build" is typed
     new HtmlWebpackPlugin({
       title: 'Shape Tracker',
       template: './src/index.html',
